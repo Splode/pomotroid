55ad87738f6dcc85cc0fe39cc0cda177383f5658
Christopher Murphy <christopherianmurphy@gmail.com>
2018-02-28 16:57:43 -0700

    Increased click area for titlebar buttons

--- src/renderer/components/Titlebar.vue
+++ src/renderer/components/Titlebar.vue
@@ -1,28 +1,34 @@
 <template>
-  <nav class="Container Titlebar">
-    <div class="Titlebar-icon-wrapper">
-      <!-- menu -->
-      <div class="Icon Menu-wrapper" :class="drawerOpen ? 'is-collapsed' : ''" @click="toggleDrawer">
+  <nav class="Titlebar">
+    <!-- menu -->
+    <div class="Titlebar-icon-wrapper Titlebar-icon-wrapper--lg" style="position: absolute;" @click="toggleDrawer">
+      <div class="Icon Menu-wrapper" :class="drawerOpen ? 'is-collapsed' : ''">
         <div class="Menu-line"></div>
         <div class="Menu-line"></div>
       </div>
     </div>
+
     <h1 class="Title">Pomotroid</h1>
-    <div class="Titlebar-icon-wrapper">
-      <!-- minimize -->
-      <div class="Icon-wrapper" @click="winMinimize">
-        <svg version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-      x="0px" y="0px" viewBox="0 0 14 2" xml:space="preserve" width="15px" height="20px" class="Icon Icon--minimize">
-          <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="1" y1="1" x2="13" y2="1"/>
-        </svg>
+
+    <div style="display: flex; position: absolute; top: 0; right: 0;">
+      <div class="Titlebar-icon-wrapper Titlebar-icon-wrapper--sm" @click="winMinimize">
+        <!-- minimize -->
+        <div class="Icon-wrapper">
+          <svg version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px" y="0px" viewBox="0 0 14 2" xml:space="preserve" width="15px" height="20px" class="Icon Icon--minimize">
+            <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="1" y1="1" x2="13" y2="1"/>
+          </svg>
+        </div>
       </div>
-      <!-- close -->
-      <div class="Icon-wrapper" @click="winClose">
-        <svg version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-      x="0px" y="0px" viewBox="0 0 12.6 12.6" xml:space="preserve" height="15px" class="Icon Icon--close">
-          <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="1" y1="1" x2="11.6" y2="11.6"/>
-          <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="11.6" y1="1" x2="1" y2="11.6"/>
-        </svg>
+      <div class="Titlebar-icon-wrapper Titlebar-icon-wrapper--sm" @click="winClose">
+        <!-- close -->
+        <div class="Icon-wrapper">
+          <svg version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px" y="0px" viewBox="0 0 12.6 12.6" xml:space="preserve" height="15px" class="Icon Icon--close">
+            <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="1" y1="1" x2="11.6" y2="11.6"/>
+            <line fill="none" stroke="#F6F2EB" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" x1="11.6" y1="1" x2="1" y2="11.6"/>
+          </svg>
+        </div>
       </div>
     </div>
   </nav>
@@ -56,11 +62,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.Icon-wrapper {
-  &:hover .Icon--close line, &:hover .Icon--minimize line {
-    stroke: $colorRed;
-  }
-}
 
 .Icon--close, .Icon--minimize {
   & line {
@@ -85,9 +86,6 @@ export default {
   flex-direction: column;
   justify-content: space-around;
   height: 80%;
-  &:hover .Menu-line {
-    background-color: $colorRed;
-  }
   &.is-collapsed {
     & .Menu-line:first-child {
       transform: rotate(-45deg);
@@ -104,14 +102,14 @@ export default {
   color: $colorGreen;
   font-size: 1rem;
   font-weight: 200;
+  padding-top: 18px;
 }
 
 .Titlebar {
-  align-items: flex-start;
-  display: flex;
-  justify-content: space-between;
   letter-spacing: .05em;
-  padding-top: 18px;
+  margin-bottom: 18px;
+  position: relative;
+  text-align: center;
   height: 50px;
   -webkit-app-region: drag;
 }
@@ -120,7 +118,22 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 45px;
+  width: 20px;
   height: 20px;
+  -webkit-app-region: no-drag;
+  &:hover .Menu-line {
+    background-color: $colorRed;
+  }
+  &:hover .Icon--close line, &:hover .Icon--minimize line {
+    stroke: $colorRed;
+  }
+}
+
+.Titlebar-icon-wrapper--sm {
+  padding: 18px 9px;
+}
+
+.Titlebar-icon-wrapper--lg {
+  padding: 18px;
 }
 </style>
