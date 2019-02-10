f130ef604dc092f8bb9ce18c017a8c1650113f4b
Christopher Murphy <christopher@pop-os.localdomain>
2019-02-09 21:50:33 -0700

    docs: add documentation for Timer

--- src/renderer/utils/timer.js
+++ src/renderer/utils/timer.js
@@ -1,11 +1,31 @@
 import { EventBus } from './event-bus'
 
-export default class {
+/**
+ * Creates a new instance of a Timer.
+ * The Timer manages the timers for the application via setInterval.
+ *
+ * @export
+ * @class Timer
+ */
+export default class Timer {
+  /**
+   *Creates an instance of Timer.
+   * @param {number} min - The number of minutes to countdown.
+   * @memberof Timer
+   */
   constructor(min) {
     this.time = 0
     this.totalSeconds = min * 60
   }
 
+  /**
+   * Start the timer by creating a new timer.
+   *
+   * @memberof Timer
+   * @emits 'timer-started' - Emitted when the timer has started.
+   * @emits 'timer-advanced' - Emits current time and total time at interval.
+   * @emits 'timer-completed' - Emitted when the timer has completed.
+   */
   start() {
     if (!this.timerInt) {
       this.timerInt = setInterval(() => {
@@ -21,12 +41,24 @@ export default class {
     }
   }
 
+  /**
+   * Pause the timer.
+   * Clears and removes current interval.
+   *
+   * @memberof Timer
+   */
   pause() {
     clearInterval(this.timerInt)
     delete this.timerInt
     EventBus.$emit('timer-paused')
   }
 
+  /**
+   * Reset the current timer.
+   * Clears and removes the current interval and resets time.
+   *
+   * @memberof Timer
+   */
   reset() {
     clearInterval(this.timerInt)
     delete this.timerInt
@@ -34,6 +66,11 @@ export default class {
     EventBus.$emit('timer-reset')
   }
 
+  /**
+   * Resume the timer from a paused state.
+   *
+   * @memberof Timer
+   */
   resume() {
     if (!this.timerInt) {
       this.start()
