b98c54941fe89162c6001d4b9d74d2a53157b1f2
winktim <tim.winkelmann@divtec.ch>
2019-05-17 16:03:30 +0100

    Replaced setInterval with setTimeout to prevent lagging behind

--- src/renderer/utils/timer.js
+++ src/renderer/utils/timer.js
@@ -8,27 +8,41 @@ export default class {
 
   start() {
     if (!this.timerInt) {
-      this.timerInt = setInterval(() => {
+      // we are not calling back on fixed seconds, but on fixed seconds plus this offset
+      const msOffset = new Date().getMilliseconds()
+
+      const timerLoop = () => {
         this.time += 1
         if (this.time >= this.totalSeconds) {
           this.pause()
           EventBus.$emit('timer-completed')
+          // return to prevent the next timeout
+          return
         } else {
           EventBus.$emit('timer-advanced', this.time, this.totalSeconds)
         }
-      }, 1000)
+
+        // compute how many ms to wait before the next call
+        // we do this because the callback takes time, so calling with 1000 ms of delay each time
+        // makes us lag behind after a bit
+        const computedTimeout = 1000 - (new Date().getMilliseconds() - msOffset)
+        this.timerInt = setTimeout(timerLoop, computedTimeout)
+      }
+
+      // first call is always in 1000 ms
+      this.timerInt = setTimeout(timerLoop, 1000)
       EventBus.$emit('timer-started')
     }
   }
 
   pause() {
-    clearInterval(this.timerInt)
+    clearTimeout(this.timerInt)
     delete this.timerInt
     EventBus.$emit('timer-paused')
   }
 
   reset() {
-    clearInterval(this.timerInt)
+    clearTimeout(this.timerInt)
     delete this.timerInt
     this.time = 0
     EventBus.$emit('timer-reset')
